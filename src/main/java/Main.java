76209d8e662cf2506b3d2a513fe254b9dc5507aa
srinivas <mr.seenu9392@gmail.com>
2024-06-18 16:12:04 +0530

    stage1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        System.out.println(input+":command not found");
     }
 }
