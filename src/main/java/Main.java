505330ec9a9cfcb138537441faa43fa9da528d71
srinivas <mr.seenu9392@gmail.com>
2024-06-19 22:24:37 +0530

    afe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,23 @@
-// Uncomment this block to pass the first stage
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-        System.out.print("$ ");
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input+": command not found");
+        
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+            
+            if (input.equals("exit 0")) {
+                break;
+            } 
+            System.out.println(input + ": command not found");
 
-        do{   
-        System.out.print("$ "); 
-        input= scanner.nextLine();
-
-        if(input.equals("exit 0")){
-            break;
+            if (input.startsWith("echo ")) {
+                System.out.println(input.substring(5));
+            } else {
+                System.out.println(input + ": command not found");
+            }
         }
-        else if(input.startsWith("echo")){
-            System.out.println(input.substring(5));
-        }
-        else{
-            System.out.println(input+" :command not found");
-        }
-        }
-        while(true);
     }
 }
