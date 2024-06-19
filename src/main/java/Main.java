060b0ff611f73b0c9a14cf56f18db179ec4ddde4
srinivas <mr.seenu9392@gmail.com>
2024-06-19 22:30:37 +0530

    ave

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        while (true) {
-            System.out.print("$ ");
-            String input = scanner.nextLine();
-            
-            if (input.equals("exit 0")) {
-                break;
-            } 
-            System.out.println(input + ": command not found");
-
-            if (input.startsWith("echo ")) {
-                System.out.println(input.substring(5));
-            } else {
-                System.out.println(input + ": command not found");
-            }
-        }
+  public static void main(String[] args) throws Exception {
+    // System.out.println("Logs from your program will appear here!");
+    while (true) {
+      System.out.print("$ ");
+      Scanner scanner = new Scanner(System.in);
+      String input = scanner.nextLine();
+      if (input.equals("exit 0")) {
+        break;
+      }
+      System.out.println(input + ": command not found");
+      if (input.startsWith("echo")) {
+        System.out.println(input.substring(5));
+      } else {
+        System.out.println(input + ": command not found");
+      }
     }
+  }
 }
