7486f7047074ee3ff9c3e9720ae7c0c98d95b6aa
srinivas <mr.seenu9392@gmail.com>
2024-06-21 11:40:59 +0530

    gef

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,45 @@
+import java.io.File;
 import java.util.Scanner;
 import java.util.Set;
+
 public class Main {
-  public static void main(String[] args) throws Exception {
-    // System.out.println("Logs from your program will appear here!");
-    Set<String> commands =Set.of("exit","type","echo");
-    while (true) {
-      System.out.print("$ ");
-      Scanner scanner = new Scanner(System.in);
-      String input = scanner.nextLine();
-      if (input.equals("exit 0")) {
-        break;
-      }
-      else if (input.startsWith("echo ")) {
-        System.out.println(input.substring(5));
-      } 
-      else if(input.startsWith("type ")){
-        String term=input.substring(5).trim();
-        if(commands.contains(term)){
-          System.out.println(term+" is a shell builtin");
-        }else{
-          System.out.println(term+": not found");
+    public static void main(String[] args) throws Exception {
+        Set<String> commands = Set.of("exit", "type", "echo");
+        String[] paths = System.getenv("PATH").split(File.pathSeparator);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+
+            if (input.equals("exit 0")) {
+                break;
+            } else if (input.startsWith("echo ")) {
+                System.out.println(input.substring(5));
+            } else if (input.startsWith("type ")) {
+                String term = input.substring(5).trim();
+                if (commands.contains(term)) {
+                    System.out.println(term + " is a shell builtin");
+                } else {
+                    boolean fileFound = false;
+                    for (String path : paths) {
+                        File file = new File(path, term);
+                        if (file.exists()) {
+                            fileFound = true;
+                            System.out.println(term + " is " + file.getAbsolutePath());
+                            break;
+                        }
+                    }
+                    if (!fileFound) {
+                        System.out.println(term + ": not found");
+                    }
+                }
+            } else {
+                System.out.println(input + ": command not found");
+            }
         }
-               
-      }
-      else {
-        System.out.println(input + ": command not found");
-      }
 
-
-
+    
     }
-  }
 }
